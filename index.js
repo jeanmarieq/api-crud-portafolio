641283d49ca2dbc03a94ddb080e50ac12f66b823
Dario Gonzalez <jdariogonzalez.2020@gmail.com>
2021-06-19 05:33:11 -0300

    commit inicial creada api-crud y agregada algunas dependencias

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'my-database.bd'
+  });
+
+app.set('view engine', 'ejs')
+
+app.get('/fotos', (req, res) => {
+    res.send('')
+})
+
+
+
+app.listen(8080, () => {
+    console.log('Servidor iniciado na porta 8080')
+})
